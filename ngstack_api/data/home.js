eb0a403ab515f0670c2c60f31212d4154b7e0e14
C. Hoffmann <caiohoffmann@gmail.com>
2019-10-19 10:05:52 -0500

    Data corrections

---
+++ ngstack_api/data/home.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Home = new mongoose.Schema({
+    title: { type: String, required: true, index: true },
+    tags: [String],
+    owner: {
+        name: { type: String, required: true },
+        badge: Number
+    },
+    date: { type: Date, default: Date.now },
+});
+
+Posts.index({ title: 1, 'owner.name': 1 }, { unique: true });
+Posts.index({ tags: 1 });
+
+module.exports = mongoose.model('home', Home);
--- ngstack_api/data/post.js
+++ ngstack_api/data/post.js
@@ -3,13 +3,20 @@ const mongoose = require('mongoose');
 const Posts = new mongoose.Schema({
     title: { type: String, required: true, index: true },
     tags: [String],
-    owner: {
-        name: { type: String, required: true },
-        badge: Number
-    },
     date: { type: Date, default: Date.now },
+    comments: [{
+        content: { type: String, required: true },
+        like: String,
+        replies: [{
+            content: String,
+            likes: Number,
+            owner: String
+        }],
+        updated: { type: Date, default: Date.now }
+    }]
 });
 
-Posts.index({ title: 1, 'owner.name': 1 }, { unique: true });
+
+
 
 module.exports = mongoose.model('post', Posts);
