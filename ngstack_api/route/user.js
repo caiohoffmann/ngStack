d457273cde3734728b567705164acc42751a4bf7
MilkiasRy <milkiasghebremariam@gmail.com>
2019-10-19 18:59:04 -0500

    add user route

--- ngstack_api/route/user.js
+++ ngstack_api/route/user.js
@@ -3,37 +3,94 @@ const express = require('express');
 const { verifyToken } = require('../utils/verifyToken');
 const User=require('../data/user');
 const router = express();
+const response = require('../utils/response');
+
+
+//router.use('/:userId/user', getUser, replyRouter);
+
+
+//###################################################
 
 router.get('/', verifyToken, async (req, res) => {
-    const list = await User.find({}).exec();
-    res.json(list);
+    const user = await User.find({}).exec();
+    res.json(response(user));
+
+
+    //  res.json(response(comment));
 });
 
 
+//###########################################################
+
 router.post('/', verifyToken, async (req, res) => {
+    if(err)throw err;
     const u = new User({
         email: req.body.email,
         password: req.body.password,
         name: req.body.name
     });
+ 
     const user = await u.save();
-    res.json(user);
+    res.json(response(user));
 });
+
+//################################################################
 //edit
 router.patch('/:id',verifyToken,async(req,res)=>{
     let id=req.params.id;
-    let newtype=req.body.type;
+    let newtype=req.body.name;
     let myquery={
         _id:id
-    }
+    };
 
+    const newValue={$set:{name:newtype}};
+    
+    const user= await User.updateOne(myquery,newValue,(err,success)=>{
+    if(err)throw err;
+    
+    res.json(response(user))
+})
 
 })
 
-router.delete('/:id', verifyToken, async (req, res) => {
+//#######################################################################
+
+router.delete('delete/:id', verifyToken, async (req, res) => {
     let userId = req.params.id;
-    const usser = await User.deleteOne({ _id: userId });
-    res.json({ msg: 'successfuly deleted' })
+    const user = await User.deleteOne({ _id: userId });
+    res.json(response(user))
 });
+//########################################
+
+router.put('/:id',async(req,res)=>{
+    let userId = req.params.id;
+    let newbodytype=req.body.type;
+    let databd={
+        'id':userId
+    }
+    let newdata={$set:{type:newbodytype}};
+    const user=await User.updateOne(databd,newdata,(err,success)=>{
+        if(err)throw err;
+        res.json(response(user))
+    })
+})
+
+
+
+
+
+
+//for login
+// router.post('/login',async(req,res,next )=>{
+// let username=req.body.username;
+// let password=req.body.password;
+// const user=await User.find({
+//     "username":username,
+//     "password":password
+// });
+
+
+
+// })
 
 module.exports = router;
