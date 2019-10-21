ca2f19d46857f6ab6e7811d042e7443338028633
MilkiasRy <milkiasghebremariam@gmail.com>
2019-10-20 19:41:56 -0500

    loginsignupIntialUI

--- ngStack/src/app/app.module.ts
+++ ngStack/src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { SignUpComponent } from './core/login/signup.componet';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
@@ -19,16 +21,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './core/login/login.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     StoreModule.forRoot({ user: reducer }),
+
 
     MatSliderModule,
     MatTabsModule,
@@ -37,11 +42,12 @@ import { LoginComponent } from './core/login/login.component';
     // Material
     MatButtonModule,
     MatIconModule,
-
+    MatFormFieldModule,
+    MatInputModule,
 
     EffectsModule.forRoot([UsersEffects])
   ],
   providers: [UsersService, UsersStoreFacade],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, LoginComponent, SignUpComponent],
 })
 export class AppModule { }
--- ngStack/src/app/core/login/login.component.ts
+++ ngStack/src/app/core/login/login.component.ts
@@ -2,12 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  template: ``,
+  template: `<form>
+  <mat-form-field>
+    <mat-label>First name</mat-label>
+    <input matInput (cdkAutofill)="userNameAutofilled = $event.isAutofilled">
+    <mat-hint *ngIf="userNameAutofilled">Autofilled!</mat-hint>
+  </mat-form-field>
+  <br>
+  <mat-form-field>
+    <mat-label>Password</mat-label>
+    <input matInput  type="password" (cdkAutofill)="passwordAutofilled = $event.isAutofilled">
+    <mat-hint *ngIf="passwordAutofilled">Autofilled!</mat-hint>
+  </mat-form-field><br>
+  <button mat-raised-button>login</button>
+</form>`,
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-
+  userAutofilled: boolean;
+  passwordAutofilled: boolean;
   constructor() { }
 
   ngOnInit() {
