c20382da2e2c550bd04c0e0f8823f29f6eba560b
alimohammadahmadi <alimohammad.ahmadi2008@gmail.com>
2019-10-21 12:10:16 -0500

    home route added to the navbar

--- ngStack/src/app/app.routes.ts
+++ ngStack/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppHeaderComponent } from './core/header/app.header';
 import { CommentsComponent } from './core/comments/app.comments';
 import { LoginComponent } from './core/login/login.component'
+import { HomeComponent } from './core/home/home.component';
 
 
 const routes: Routes = [
   { path: 'comments', component: CommentsComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/comments', pathMatch: 'full' }
+  { path: '', redirectTo: '/comments', pathMatch: 'full' },
+  { path: 'home' , component: HomeComponent}
 ];
 export const globalroute = RouterModule.forRoot(routes);
