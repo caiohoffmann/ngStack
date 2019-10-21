b29c7e505f5a3e2f927a58d4f60064ce0fe85805
alimohammadahmadi <alimohammad.ahmadi2008@gmail.com>
2019-10-21 11:43:54 -0500

    Home page UI updated

--- ngStack/src/app/core/home/home.component.ts
+++ ngStack/src/app/core/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {FormGroup, FormBuilder, Validators,FormControl,FormArray} from '@angular/forms';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,39 +11,35 @@ import {FormGroup, FormBuilder, Validators,FormControl,FormArray} from '@angular
 export class HomeComponent implements OnInit {
   homes: Object;
   myForm: FormGroup;
-  mytags = ["Art", "Tech", "Food", "Angular"];
+  mytags= ["Tech", "Food", "Art", "Angular"];
+  
 
-  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
-
+  constructor(private http: HttpClient, private fb: FormBuilder) {
     this.http.get('http://localhost:3000/homes', {headers:{"ngstackauth": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NzE1MzEyOTh9.Ihtu4elMUcxb9e37VQk9oYxpyoohJwPfoBkQNgS0qWU"}}).subscribe(res=>{
       // localStorage.setItem('data', JSON.stringify(res));
       this.homes = res;
-    })
-
-    this.myForm = formBuilder.group({
+    });
+    this.myForm = this.fb.group({
       'title':['', Validators.required],
-      'tags': this.addtagscontroller()
-    })
+      'tags': this.fb.array(this.mytags.map(x=>!1))
+    });
     this.myForm.valueChanges.subscribe((val)=>{
       console.log(val);
     })
    }
 
-  addtagscontroller(){
-    const arr = this.mytags.map(ele=>{
-      return this.formBuilder.control(false);
-    })
-  }
-
-  get TagsArray(){
-    return <FormArray>this.myForm.get('tags');
-  }
-
   ngOnInit() {
   }
 
   onSubmit(){
-    console.log(this.myForm)
+    const valueToStore = Object.assign({}, this.myForm.value, {
+      tags: this.convertToValue('tags')
+    });
+    console.log(valueToStore);
+  }
+
+  convertToValue(key: string) {
+    return this.myForm.value[key].map((x, i) => x && this.mytags[i]).filter(x => !!x);
   }
 
 }
