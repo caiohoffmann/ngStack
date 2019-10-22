7b5dc68cf04f82fd29f350a866758b884b887f50
johnstyvesvallon <styvesGIT12345!>
2019-10-21 20:42:26 -0500

    Add the guard for post page

---
+++ ngStack/src/app/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router'
+import { UsersStoreFacade } from '../store/users/users.store-facade';
+import { variable } from '@angular/compiler/src/output/output_ast';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+  tokenAuth;
+  constructor(private _router: Router, private _userFacade: UsersStoreFacade) { }
+  CanActivate(): boolean {
+    this._userFacade.getToken().subscribe(res => {
+      this.tokenAuth = res;
+    });
+    if (this.tokenAuth) {
+      return true
+    }
+    else {
+      this._router.navigate(['/login']);
+      return false;
+    }
+  }
+
+}
+
